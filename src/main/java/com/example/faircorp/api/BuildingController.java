fa0da9262ba09789ee24d3c3e131f6c5e1ed042c
Mohsen Hadavi <moh3nhadavi@gmail.com>
2022-11-14 20:09:23 +0100

    building route bug fixed

--- src/main/java/com/example/faircorp/api/BuildingController.java
+++ src/main/java/com/example/faircorp/api/BuildingController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin
 @RestController
-@RequestMapping("/buildings")
+@RequestMapping("/api/buildings")
 public class BuildingController {
     private final BuildingDao buildingDao;
 
